cbe8e0c0deeda305c8ac4de9bc7d003880e66e7b
Attila BODY <attila.body@gmail.com>
2016-11-22 08:24:07 +0100

    WIP

--- inc/i2cmaster.h
+++ inc/i2cmaster.h
@@ -3,24 +3,89 @@
 #include <stm32_hal.h>
 #include <inttypes.h>
 
-class I2cMaster
+extern "C" {
+void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c);
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c);
+void HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c);
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c);
+void HAL_I2C_MemTxCpltCallback(I2C_HandleTypeDef *hi2c);
+void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c);
+void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c);
+}
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////
+class I2cCallbackDispatcher
+{
+public:
+	static I2cCallbackDispatcher& Instance() { return m_instance; }
+
+	class II2cCallback
+	{
+	public:
+		enum CallbackType
+		{
+			None,
+			MasterTxCpltCallback,
+			MasterRxCpltCallback,
+			SlaveTxCpltCallback,
+			SlaveRxCpltCallback,
+			MemTxCpltCallback,
+			MemRxCpltCallback,
+			ErrorCallback
+		};
+
+	private:
+		virtual void I2cCallback(I2C_HandleTypeDef *hi2c, CallbackType type) = 0;
+		friend class I2cCallbackDispatcher;
+	};
+
+	void Register(II2cCallback *handler);
+	void Callback(I2C_HandleTypeDef *hi2c, II2cCallback::CallbackType type);
+
+private:
+	I2cCallbackDispatcher() {};
+	I2cCallbackDispatcher(const I2cCallbackDispatcher &rhs) = delete;
+
+	static I2cCallbackDispatcher	m_instance;
+
+	II2cCallback 					*m_handlers[2] {0, 0};
+};
+
+//////////////////////////////////////////////////////////////////////////////
+//
+//////////////////////////////////////////////////////////////////////////////
+class I2cMaster : public I2cCallbackDispatcher::II2cCallback
 {
 public:
 	typedef HAL_StatusTypeDef Status;
 
-	I2cMaster(I2C_HandleTypeDef *hi2c) : m_hi2c(hi2c) {}
+	I2cMaster(I2C_HandleTypeDef *hi2c);
 	inline Status Write(uint8_t data, const uint16_t address);
 	inline Status Read(uint8_t &data, const uint16_t address);
 
 protected:
-	I2C_HandleTypeDef	*m_hi2c;
+	virtual void I2cCallback(I2C_HandleTypeDef *hi2c, CallbackType type);
+	//virtual ~I2cMaster();	//will never be destructed
+
+	I2C_HandleTypeDef			*m_hi2c;
+	volatile CallbackType		m_expectedCallback = None;
+
+	enum Mode { Poll, Interrupt, Dma }	m_mode;
+	inline void WaitCallback();
 };
 
+//////////////////////////////////////////////////////////////////////////////
 inline I2cMaster::Status I2cMaster::Write(uint8_t data, const uint16_t address)
 {
 	return HAL_I2C_Master_Transmit(m_hi2c, address, &data, 1, HAL_MAX_DELAY);
 }
 
+//////////////////////////////////////////////////////////////////////////////
 inline I2cMaster::Status I2cMaster::Read(uint8_t &data, const uint16_t address)
 {
 	return HAL_I2C_Master_Receive(m_hi2c, address, &data, 1, HAL_MAX_DELAY);
--- src/i2cmaster.cpp
+++ src/i2cmaster.cpp
@@ -5,6 +5,75 @@
  *      Author: compi
  */
 
+#include <i2cmaster.h>
+
+void HAL_I2C_MasterTxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	I2cCallbackDispatcher::Instance().Callback(hi2c, I2cCallbackDispatcher::II2cCallback::MasterTxCpltCallback);
+}
+
+void HAL_I2C_MasterRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	I2cCallbackDispatcher::Instance().Callback(hi2c, I2cCallbackDispatcher::II2cCallback::MasterRxCpltCallback);
+}
 
 
+void HAL_I2C_SlaveTxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	I2cCallbackDispatcher::Instance().Callback(hi2c, I2cCallbackDispatcher::II2cCallback::SlaveTxCpltCallback);
+}
 
+void HAL_I2C_SlaveRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	I2cCallbackDispatcher::Instance().Callback(hi2c, I2cCallbackDispatcher::II2cCallback::SlaveRxCpltCallback);
+}
+
+void HAL_I2C_MemTxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	I2cCallbackDispatcher::Instance().Callback(hi2c, I2cCallbackDispatcher::II2cCallback::MemTxCpltCallback);
+}
+
+void HAL_I2C_MemRxCpltCallback(I2C_HandleTypeDef *hi2c)
+{
+	I2cCallbackDispatcher::Instance().Callback(hi2c, I2cCallbackDispatcher::II2cCallback::MemRxCpltCallback);
+}
+
+void HAL_I2C_ErrorCallback(I2C_HandleTypeDef *hi2c)
+{
+	I2cCallbackDispatcher::Instance().Callback(hi2c, I2cCallbackDispatcher::II2cCallback::ErrorCallback);
+}
+
+
+I2cCallbackDispatcher I2cCallbackDispatcher::m_instance;
+
+void I2cCallbackDispatcher::Register(II2cCallback *handler)
+{
+	if(!m_handlers[0])
+		m_handlers[0] = handler;
+	else if(!m_handlers[1])
+		m_handlers[1] = handler;
+}
+
+void I2cCallbackDispatcher::Callback(I2C_HandleTypeDef *hi2c, II2cCallback::CallbackType type)
+{
+	if(m_handlers[0])
+		m_handlers[0]->I2cCallback(hi2c, type);
+}
+
+I2cMaster::I2cMaster(I2C_HandleTypeDef *hi2c)
+: m_hi2c(hi2c)
+{
+	I2cCallbackDispatcher::Instance().Register(this);
+}
+
+void I2cMaster::I2cCallback(I2C_HandleTypeDef *hi2c, CallbackType type)
+{
+	if(m_expectedCallback == type || type == ErrorCallback ) {
+		m_expectedCallback = None;
+	}
+}
+
+inline void I2cMaster::WaitCallback()
+{
+	while(m_expectedCallback != None) {}
+}
