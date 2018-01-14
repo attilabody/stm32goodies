047b8c48c8e585a9b5c011678089368a434a06d3
Attila BODY <attila.body@gmail.com>
2018-01-14 12:34:56 +0100

    bluepill: Singleton<> moved to st32goodies

---
+++ inc/sg/singleton.h
@@ -0,0 +1,35 @@
+/*
+ * Singleton.h
+ *
+ *  Created on: Jan 13, 2018
+ *      Author: compi
+ */
+
+#ifndef SINGLETON_H_
+#define SINGLETON_H_
+
+#include <sg/itlock.h>
+namespace sg {
+
+template<typename T> class Singleton
+{
+protected:
+	Singleton() {};
+
+public:
+	static T& Instance() {
+		static T instance;
+		return instance;
+	}
+};
+
+template<typename T> struct SafeSingletonInitializer
+{
+	SafeSingletonInitializer() {
+		sg::ItLock	l;
+		T::Instance();
+	}
+};
+}	//	namespace sg
+
+#endif /* SINGLETON_H_ */
